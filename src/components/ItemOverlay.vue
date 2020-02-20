87953a11ddf3ee7c5eb304a57ccfd1ceeca048af
Jeff Villarina <jeffvilla@Jeffs-MacBook-Pro.local>
2020-02-20 03:15:20 -0800

    add carousel images, need to add functionality

--- src/components/ItemOverlay.vue
+++ src/components/ItemOverlay.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="item-overlay">
-        {{ images }}
+        <ul class="carousel">
+            <li class="carousel-item"
+                v-for="(image, index) in images"
+                v-bind:key="index">
+                <img class="carousel-image" :src="image.href">
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -15,13 +21,31 @@ export default {
 
 <style scoped>
 .item-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: black;
-  opacity: 0.90;
-  z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: black;
+    opacity: 1;
+    z-index: 99;
 }
+.carousel {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    overflow: hidden;
+    height: 363px;
+    width: 363px;
+}
+.carousel-item {
+    height: 363px;
+    width: 363px;
+    margin: 0;
+}
+
+
 </style>
